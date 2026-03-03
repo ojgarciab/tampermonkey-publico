f38f695a46179ac7572b9eb7cdb70d9b3b551ef3
Óscar García <ojgarciab@users.noreply.github.com>
2026-03-03 07:15:39 +0100

    Cambios en URL de Teams

--- teams.reductor-altura-subtitulos.user.js
+++ teams.reductor-altura-subtitulos.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Reductor de la altura de la barra de subtítulos de Teams
 // @namespace    https://github.com/ojgarciab
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       Óscar García
 // @match        https://teams.microsoft.com/v2/*
 // @match        https://teams.microsoft.com/l/meetup-join/*
+// @match        https://teams.cloud.microsoft/v2/*
+// @match        https://teams.cloud.microsoft/l/meetup-join/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @homepageURL  https://ojgarciab.github.io/tampermonkey-publico/
 // @supportURL   https://github.com/ojgarciab/tampermonkey-publico/issues
