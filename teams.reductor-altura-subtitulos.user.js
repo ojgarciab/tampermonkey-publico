af024660d044f505f9f4e080588c94d5f22c09c7
Óscar García <ojgarciab@users.noreply.github.com>
2026-03-03 07:19:38 +0100

    Actualizado icono de Teams

--- teams.reductor-altura-subtitulos.user.js
+++ teams.reductor-altura-subtitulos.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Reductor de la altura de la barra de subtítulos de Teams
 // @namespace    https://github.com/ojgarciab
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       Óscar García
 // @match        https://teams.microsoft.com/v2/*
 // @match        https://teams.microsoft.com/l/meetup-join/*
 // @match        https://teams.cloud.microsoft/v2/*
 // @match        https://teams.cloud.microsoft/l/meetup-join/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=teams.cloud.microsoft
 // @homepageURL  https://ojgarciab.github.io/tampermonkey-publico/
 // @supportURL   https://github.com/ojgarciab/tampermonkey-publico/issues
 // @downloadURL  https://ojgarciab.github.io/tampermonkey-publico/teams.reductor-altura-subtitulos.user.js
